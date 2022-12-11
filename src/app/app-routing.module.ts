6276654caa14aca16fec2dac615a71d1cb8374a7
Pushpendra-Sapkale <84712227+Pushpendra-Sapkale@users.noreply.github.com>
2022-12-11 18:23:30 +0530

    added contact me

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren:() => import('./home-module/home-module.module').then(p=>p.HomeModuleModule)
+    loadChildren: () => import('./home-module/home-module.module').then(p => p.HomeModuleModule)
   }
 ];
 
