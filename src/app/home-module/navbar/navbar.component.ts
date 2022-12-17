7a4496c7686a85643d609d2aa7085accd26f29db
Pushpendra-Sapkale <84712227+Pushpendra-Sapkale@users.noreply.github.com>
2022-12-18 00:33:15 +0530

    removed side-nav-width for mobile screen

--- src/app/home-module/navbar/navbar.component.ts
+++ src/app/home-module/navbar/navbar.component.ts
@@ -7,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit{
   showNavItems: boolean = true;
+  hideButton: boolean = false;
 
   ngOnInit(): void{
     if(window.innerWidth < 500) {
       this.showNavItems = false;
+      this.hideButton = true;
     }
   }
 
