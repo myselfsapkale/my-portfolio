4a126e1aa9fe3b176b07433036023a6efe1a79ab
Pushpendra <dev.psapkale@gmail.com>
2023-07-24 11:07:10 +0530

    Added mobile nav with aal the configurations

--- src/app/home-module/mobile-nav/mobile-nav.component.ts
+++ src/app/home-module/mobile-nav/mobile-nav.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { Event, NavigationEnd, Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { Event, NavigationEnd, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-mobile-nav',
-  templateUrl: './mobile-nav.component.html',
-  styleUrls: ['./mobile-nav.component.css']
+  selector: "app-mobile-nav",
+  templateUrl: "./mobile-nav.component.html",
+  styleUrls: ["./mobile-nav.component.css"],
 })
 export class MobileNavComponent {
-  activeRoute: string = '';
+  activeRoute: string = "";
   showModal: boolean = false;
   constructor(private router: Router) {
     router.events.subscribe((event: Event) => {
@@ -19,18 +19,22 @@ export class MobileNavComponent {
 
   findCurrentRoute(currentPath: string): string {
     switch (true) {
-      case currentPath.indexOf('home') > -1:
-        return 'home';
-      case currentPath.indexOf('projects') > -1:
-        return 'projects';
-      case currentPath.indexOf('about-me') > -1:
-        return 'about-me';
-      case currentPath.indexOf('contact-me') > -1:
-        return 'contact-me';
-      case currentPath.indexOf('resume') > -1:
-        return 'resume';
+      case currentPath.indexOf("home") > -1:
+        return "home";
+      case currentPath.indexOf("projects") > -1:
+        return "projects";
+      case currentPath.indexOf("about-me") > -1:
+        return "about-me";
+      case currentPath.indexOf("contact-me") > -1:
+        return "contact-me";
+      case currentPath.indexOf("resume") > -1:
+        return "resume";
       default:
-        return '';
+        return "";
     }
+  }
+
+  jumpToGithub() {
+    window.open("https://github.com/pushpendra-sapkale-dev", "_blank");
   }
 }
