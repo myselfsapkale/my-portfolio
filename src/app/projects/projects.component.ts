660130eda6b998f70787782c1cea5f6d0ec32a23
Pushpendra <dev.psapkale@gmail.com>
2023-09-07 11:06:21 +0530

    responsive design
    Added responsive ness here

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CommonServiceService } from '../services/common-service.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent {
+  isDarkMode: boolean = true;
+  subscription: Subscription;
+
+
+  constructor(private _commonService: CommonServiceService) {
+    this.subscription = this._commonService.lightDarkModeEmit.subscribe((status) => {
+      this.isDarkMode = status;
+    });
+    if (window.localStorage.getItem('isDarkMode') == 'true') {
+      this.isDarkMode = true;
+    } else {
+      this.isDarkMode = false;
+    }
+  }
 
 }
